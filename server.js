667163f38aa398136f87912efb6841afd70c3e8b
Ryan Kray <rrich.kray.93@gmail.com>
2024-12-19 09:26:51 -0500

    adding cors middleware

--- server.js
+++ server.js
@@ -32,6 +32,23 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+})
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`Now listening on port ${PORT}`);
