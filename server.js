48ef7b148ce64207e78fdc5b699784e01abb17d9
Ryan Kray <rrich.kray.93@gmail.com>
2024-12-19 09:45:54 -0500

    adding cors middleware

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get("/*", (req, res) => {
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+  res.setHeader('Access-Control-Allow-Origin', 'https://comments-section-client-qagv-clmc7qzxv-rrichkrays-projects.vercel.app/');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
